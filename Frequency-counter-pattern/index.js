4babb14831f5e3d4cd45e01cbac5694b52e82cb0
Ebenezer Igbinoba <ebenezerigbinoba@Ebenezers-MacBook-Pro.local>
2022-02-20 01:21:23 +0100

    .

--- Frequency-counter-pattern/index.js
+++ Frequency-counter-pattern/index.js
@@ -14,7 +14,7 @@ const checkOccurence = (arr1, arr2) => {
 
 // console.log(checkOccurence([1, 2, 3], [4, 9]))
 
-/*** using Frequency counter algorithm***/
+/*** using Frequency counter algorithm ***/
 const reFactoredcheckOccurence = (arr1, arr2) => {
     if(arr1.length !== arr2.length)  return false
     let arrObj1 = {}
@@ -63,16 +63,16 @@ function sameFrequency(num1, num2){
   let obj2 = {}
   
   for(let char of numStr1) {
-      obj1[char] = obj1[char] + 1 || 1
+    obj1[char] = obj1[char] + 1 || 1
   }
   
   for(let char of numStr2) {
-      obj2[char] = obj2[char] + 1 || 1
+    obj2[char] = obj2[char] + 1 || 1
   } 
  for(let char in obj1) {
-     if(obj1[char] !== obj2[char]) {
+    if(obj1[char] !== obj2[char]) {
         return false
-     }
+    }
  }
  return true
 }
--- sliding-window/index.js
+++ sliding-window/index.js
@@ -39,3 +39,22 @@ function maxSubArraySum(arr, num) {
 }
 // console.log(maxSubArraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3))
 
+
+/*** Using sliding window solution ***/
+function maxSubarraySum(arr, num){
+    if(num > arr.length) return null
+    let max = arr.slice(0, num).reduce((acc,cur) => acc += cur, 0)
+    
+    let sum = max
+
+    for(let i=num; i<arr.length; i++) {
+        sum = sum + arr[i] - arr[i - num]
+        if(sum > max) {
+            max = sum
+        }
+    }
+    if(max === 0) return null
+    return max
+
+}
+console.log(maxSubarraySum([100,200,300,400], 2))
