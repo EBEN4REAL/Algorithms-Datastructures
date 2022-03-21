a07b9feca839f3f24f5b03f456f465b911de2a4e
Ebezer Igbinoba <igbinobaebenezer@gmail.com>
2022-03-21 22:57:58 +0100

    .

--- Recursion/index.js
+++ Recursion/index.js
@@ -212,3 +212,31 @@ function stringifyNumbers(obj) {
 //         }
 //     }
 // }
+
+const obj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+function collectStrings(obj) {
+    let arr = []
+    for(let key in obj) {
+        if(typeof obj[key] === 'string') {
+            arr.push(obj[key])
+        }else if(typeof obj[key] === 'object') {
+            arr = arr.concat(collectStrings(obj[key]))
+        }
+    }
+    return arr
+}
+collectStrings(obj) // ["foo", "bar", "baz"])
